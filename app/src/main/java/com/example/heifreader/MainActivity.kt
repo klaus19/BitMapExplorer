21af00def3c574c138827f6187aa093791425834
Tejas khartude <tejas.khartude@clariontechnologies.co.in>
2022-04-04 16:13:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/heifreader/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.heifreader
+
+import android.graphics.Canvas
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+
+
+
+
+}
