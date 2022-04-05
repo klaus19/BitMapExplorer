54a745b9f53d8553a1ee827eb3447617a4f088d1
Tejas khartude <tejas.khartude@clariontechnologies.co.in>
2022-04-05 16:56:58 +0530

    Applied Animation

--- app/src/main/java/com/example/heifreader/MainActivity.kt
+++ app/src/main/java/com/example/heifreader/MainActivity.kt
@@ -41,9 +41,14 @@ class MainActivity : AppCompatActivity() {
       //Creating a customised BitMap and showing it in imageView
     private fun createBitmap() {
         val bm:Bitmap = createBitmap(100,100,Bitmap.Config.ARGB_8888)
-        val canvas:Canvas = Canvas(bm)
-        canvas.drawColor(Color.BLUE)
+        val canvas = Canvas(bm)
+        canvas.drawColor(Color.MAGENTA)
+          binding.imageBitmap.visibility = View.VISIBLE
         binding.imageBitmap.setImageBitmap(bm)
+          canvas.translate(20f,25f)
+          ObjectAnimator.ofFloat(binding.imageBitmap,"translationX",110f)
+              .setDuration(500)
+              .start()
     }
 
     //Setting drawable Image into imageview programmatically
@@ -53,6 +58,10 @@ class MainActivity : AppCompatActivity() {
 
         binding.image1.visibility = View.VISIBLE
         binding.image1.setImageDrawable(drawable)
+
+    }
+
+    fun shakyAnimation(){
 
     }
 
